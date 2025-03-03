b7f645e0662f78a90cef3ef1354e1799d7e11818
José Gregorio Rincón Albarracín <jgregoriorincon@gmail.com>
2025-03-03 13:37:21 -0500

    Counter Screen

---
+++ lib/presentation/screens/counter_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatelessWidget {
+  const CounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text('10'), Text('Clicks')],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.plus_one),
+      ),
+    );
+  }
+}
